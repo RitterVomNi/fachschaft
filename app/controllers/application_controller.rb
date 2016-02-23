bd7589982f1c6261e9c82e13ab35234a8c723577
MatthiasHei <heinen.m@web.de>
2016-02-23 10:19:00 +0100

    added logic for user.studiengang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,8 +37,8 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:username, :email, :firstName, :lastName, :facebook, :img]
-    devise_parameter_sanitizer.for(:account_update) << [:username, :email, :firstName, :lastName, :facebook, :img]
+    devise_parameter_sanitizer.for(:sign_up) << [:studiengang, :email, :firstName, :lastName, :facebook, :img]
+    devise_parameter_sanitizer.for(:account_update) << [:studiengang, :email, :firstName, :lastName, :facebook, :img]
   end
 
 
